8c0b59379adecbaced77e4009a6383c7150fc788
bondparkerbond <parkerbond@gmail.com>
2015-10-10 10:51:33 -0600

    started populating views, = and scss quick fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'gem_demos/index'
-
-  get 'gem_demos/show'
-
-  get 'gem_demos/new'
-
-  get 'gem_demos/edit'
+  resources :gem_demos
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
